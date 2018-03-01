d67c356becd82adcd21cdebbed136b26efe25cd3
Hugh <hugh_gleaves@hotmail.com>
2018-03-01 07:51:37 -0700

    Comment outlining how we could do lazy paging in a non-breaking manner.

--- Octokit/Helpers/IApiPagination.cs
+++ Octokit/Helpers/IApiPagination.cs
@@ -19,5 +19,13 @@ namespace Octokit
         /// <param name="getFirstPage">A function which generates the first request</param>
         /// <param name="uri">The original URI (used only for raising an exception)</param>
         Task<IReadOnlyList<T>> GetAllPages<T>(Func<Task<IReadOnlyPagedCollection<T>>> getFirstPage, Uri uri);
+
+        // Changing the above to:
+        // Task<IReadOnlyList<T>> GetAllPages<T>(Func<Tuple<bool,Task<IReadOnlyPagedCollection<T>>>> getFirstPage, Uri uri);
+        // Would enable the implementation of 'getFirstPage' to return an indicator as to whether to run lazy or not.
+        // That could be deduced by the lambda 'getFirstPage' examint the ApiOptions for some hitherto unused value,
+        // Perhaps a start page of specifically 0 could be used here.
+        // GetAllPages could then return an instance of either ReadOnlyCollection or LazyReadOnlyCollection which we would define.
+        // Consumers would see only a IReadOnlyList...
     }
 }
